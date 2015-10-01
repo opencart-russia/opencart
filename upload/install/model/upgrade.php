1862532aad1dd370e36e70b3b0162412107ecc51
opencart-russia <forum@mail-opencart-russia.ru>
2015-10-01 21:41:56 +0400

    Update upgrade.php

--- upload/install/model/upgrade.php
+++ upload/install/model/upgrade.php
@@ -25,7 +25,7 @@ class ModelUpgrade extends Model {
 			$line = str_replace("CREATE TABLE `oc_", "CREATE TABLE `" . DB_PREFIX, $line);
 
 			// If line begins with create table we want to start recording
-			if (substr($line, 0, 12) == 'CREATE TABLE') {
+			if (substr($line, 0, 10) == 'DROP TABLE' || substr($line, 0, 12) == 'CREATE TABLE') {
 				$status = true;
 			}
 
@@ -122,7 +122,7 @@ class ModelUpgrade extends Model {
 			}
 
 			// Get Table Name
-			preg_match_all('#create\s*table\s*if\s*not\s*exists\s*`(\w[\w\d]*)`#i', $sql, $table);
+			preg_match_all('#create\s*table\s*`(\w[\w\d]*)`#i', $sql, $table);
 
 			if (isset($table[1][0])) {
 				$table_new_data[] = array(
@@ -361,6 +361,17 @@ class ModelUpgrade extends Model {
 				$this->db->query("UPDATE `" . DB_PREFIX . "setting` SET `value` = '" . $this->db->escape(json_encode($value)) . "' WHERE `setting_id` = '" . (int)$result['setting_id'] . "'");
 			}
 		}
+		
+		//  Change any serialized values to json values and restore in the DB
+		$query = $this->db->query("SELECT * FROM `" . DB_PREFIX . "module`");
+
+		foreach ($query->rows as $result) {
+			if ($result['setting']) {
+				$value = unserialize($result['setting']);
+
+				$this->db->query("UPDATE `" . DB_PREFIX . "module` SET `setting` = '" . $this->db->escape(json_encode($value)) . "' WHERE `module_id` = '" . (int)$result['module_id'] . "'");
+			}
+		}		
 
 		// Customer
 		$query = $this->db->query("SELECT * FROM `" . DB_PREFIX . "customer`");
